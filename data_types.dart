ef02098913e708856a0c1de2c6ecc8144a8dfa5d
kingslayer-001 <jeffmk.dev@google.com>
2024-04-12 10:51:23 +0300

    Week 1 Assignment - Data types (list, maps, int, string)

---
+++ data_types.dart
@@ -0,0 +1,9 @@
+void main() {
+  // a program that prints details about user.
+  List counties = ['Kiambu', 'Meru', 'Nairobi', 'Kirinyaga', 'Nyeri'];
+  Map carriers = {'John': "Lawyer", 'Mary': 'Doctor', 'Nancy': 'Pilot'};
+  int countyCode = 47;
+  String myName = 'Mercy';
+  print(
+      'Hello, My name is $myName and i come from 0$countyCode ${counties[0]} county. Various professions in our counry are $carriers');
+}
